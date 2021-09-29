0fcebb8813ff0456b19addf6e4c63762c93c4e70
WDaxo <sabrigo.c@gmail.com>
2021-09-28 23:01:23 -0300

    Actualización cosas

---
+++ w1/Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using w1.Pages.Repositorios;
+
+namespace w1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [Display(Name ="Usuario")]
+        [BindProperty]
+        [Required(ErrorMessage ="El Campo Usuario no puede estar vacío")]
+        public string NombreUsuario { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        [Display(Name ="Contraseña")]
+        [BindProperty]
+        [Required(ErrorMessage ="El Campo Contraseña no puede estar vacío")]
+        public string Password { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public ActionResult OnPost()
+        {        if (ModelState.IsValid)
+            { 
+            var usuario = this.NombreUsuario;
+            var password = this.Password;
+
+                var respo = new LoginRepositorio();
+                bool resultadoValidacion = respo.ExisteUsuario(usuario, password);
+
+                if (resultadoValidacion == true)
+                {
+                    Guid idSession = Guid.NewGuid();
+                    HttpContext.Session.SetString("idSession", idSession.ToString());
+                    return RedirectToPage("./Home");
+                }
+                ModelState.AddModelError(string.Empty, "Usuario o Contraseña inválidos");
+                return Page();
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ w1/Pages/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace w1.Pages.Repositorios
+{
+    public class LoginRepositorio
+    {
+        public bool ExisteUsuario(string NombreUsuario, string Password) 
+        {
+            bool respuesta = false;
+            string connectionString = "server=localhost;database=w1; Integrated Security = true";
+            using SqlConnection sql = new SqlConnection(connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_check_usuario", sql);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@NombreUsuario", NombreUsuario)); 
+            cmd.Parameters.Add(new SqlParameter("@Password", Password));
+            sql.Open();
+            int resultadoQuery = (int)cmd.ExecuteScalar();
+            if (resultadoQuery > 0) 
+            {
+                respuesta = true;
+            }
+
+
+            return respuesta;
+        }
+    }
+}
